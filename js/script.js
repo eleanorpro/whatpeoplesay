2f08aa0d27eef910cdde5f19c29c33ea8910c4d0
Ubuntu <ubuntu@ip-172-31-86-33.ec2.internal>
2020-03-09 11:12:11 +0000

    Rough homepage with google map and search box added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var map;
+var service;
+var infowindow;
+
+function initMap() {
+
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 54.933454, lng: -4.207763 },
+        zoom: 5
+    });
+
+
+}
