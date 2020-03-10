35731be226793aa48c229ad4c7e28d223796346d
Ubuntu <ubuntu@ip-172-31-86-33.ec2.internal>
2020-03-10 11:56:57 +0000

    Autocomplete search bar works and Google Map shows on main page.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
-var map;
-var service;
-var infowindow;
+//Load Google Map and enable Autocomplete
+
+function initialize() {
+    initMap();
+    initAutocomplete();
+}
+var map, marker;
 
 function initMap() {
-
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 54.933454, lng: -4.207763 },
-        zoom: 5
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 54.9783,
+            lng: -1.6178
+        },
+        zoom: 13
     });
+}
 
 
+let autocomplete;
+
+function initAutocomplete() {
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'), {
+            types: ['establishment'],
+            componentRestrictions: { 'country': ['UK'] },
+            fields: ['place_id', 'geometry', 'name']
+        });
+        
+        autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        document.getElementById('autocomplete').placeholder = 'Enter a place';
+    } else {
+        document.getElementById('details').innerHTML = place.name;
+    }
 }
